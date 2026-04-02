cc1c27f294c6bf80c2138e5b609dfd4fab884178
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2026-04-02 21:14:49 +0900

    update

--- src/umoru_arm/motion_client.py
+++ src/umoru_arm/motion_client.py
@@ -11,24 +11,37 @@ class MotionClient(object):
             namespace = "rarm"
             self.r_robot_model = RobotModel()
             self.r_robot_model.load_urdf_from_robot_description(
-                namespace + "/robot_description_viz"
+                namespace + "/robot_description"
             )
             self.r_ri = KXRROSRobotInterface(  # NOQA
                 self.r_robot_model, namespace=namespace, controller_timeout=60.0
             )
             self.r_robot_model.angle_vector(self.r_ri.angle_vector())
+            self.r_ri.send_stretch(127)
             self.r_ri.servo_on()
         if arm == "both" or arm == "larm":
             namespace = "larm"
             self.l_robot_model = RobotModel()
             self.l_robot_model.load_urdf_from_robot_description(
-                namespace + "/robot_description_viz"
+                namespace + "/robot_description"
             )
             self.l_ri = KXRROSRobotInterface(  # NOQA
                 self.l_robot_model, namespace=namespace, controller_timeout=60.0
             )
             self.l_robot_model.angle_vector(self.l_ri.angle_vector())
+            self.l_ri.send_stretch(127)
             self.l_ri.servo_on()
+
+    def wait_interpolation(self):
+        if self.mode == "both":
+            while self.l_ri.is_interpolating() and self.r_ri.is_interpolating():
+                rospy.sleep(0.1)
+        elif self.mode == "larm":
+            while self.l_ri.is_interpolating():
+                rospy.sleep(0.1)
+        elif self.mode == "rarm":
+            while self.r_ri.is_interpolating():
+                rospy.sleep(0.1)
 
     def reset_pose(self, velocity=300):
         if self.mode == "both" or self.mode == "rarm":
@@ -39,8 +52,7 @@ class MotionClient(object):
             self.l_robot_model.joint_pitch.joint_angle(np.deg2rad(0))
             self.l_robot_model.joint_yaw.joint_angle(np.deg2rad(135))
             self.l_ri.angle_vector(self.l_robot_model.angle_vector(), time=1.5)
-        while self.l_ri.is_interpolating() or self.r_ri.is_interpolating():
-            rospy.sleep(0.1)
+        self.wait_interpolation()
 
     def init_pose(self, velocity=300):
         if self.mode == "both" or self.mode == "rarm":
@@ -51,28 +63,25 @@ class MotionClient(object):
             self.l_robot_model.joint_pitch.joint_angle(np.deg2rad(-90))
             self.l_robot_model.joint_yaw.joint_angle(np.deg2rad(135))
             self.l_ri.angle_vector(self.l_robot_model.angle_vector(), time=1.5)
-        while self.l_ri.is_interpolating() or self.r_ri.is_interpolating():
-            rospy.sleep(0.1)
+        self.wait_interpolation()
 
-    def hug(self, velocity=300):
+    def hug(self, velocity=500):
         if self.mode == "both" or self.mode == "rarm":
-            self.r_robot_model.joint_yaw.joint_angle(np.deg2rad(-50))
+            self.r_robot_model.joint_yaw.joint_angle(np.deg2rad(-60))
             self.r_ri.angle_vector(self.r_robot_model.angle_vector(), time=1.5)
         if self.mode == "both" or self.mode == "larm":
-            self.l_robot_model.joint_yaw.joint_angle(np.deg2rad(50))
+            self.l_robot_model.joint_yaw.joint_angle(np.deg2rad(20))
             self.l_ri.angle_vector(self.l_robot_model.angle_vector(), time=1.5)
-        while self.l_ri.is_interpolating() or self.r_ri.is_interpolating():
-            rospy.sleep(0.1)
+        self.wait_interpolation()
 
-    def extend(self, velocity=300):
+    def extend(self, velocity=500):
         if self.mode == "both" or self.mode == "rarm":
             self.r_robot_model.joint_yaw.joint_angle(np.deg2rad(-135))
             self.r_ri.angle_vector(self.r_robot_model.angle_vector(), time=1.5)
         if self.mode == "both" or self.mode == "larm":
             self.l_robot_model.joint_yaw.joint_angle(np.deg2rad(135))
             self.l_ri.angle_vector(self.l_robot_model.angle_vector(), time=1.5)
-        while self.l_ri.is_interpolating() or self.r_ri.is_interpolating():
-            rospy.sleep(0.1)
+        self.wait_interpolation()
 
     def raise_arm(self, arm, velocity=300):
         if self.mode == "both" or self.mode == "rarm":
@@ -83,6 +92,4 @@ class MotionClient(object):
             self.l_robot_model.joint_pitch.joint_angle(np.deg2rad(90))
             self.l_robot_model.joint_yaw.joint_angle(np.deg2rad(135))
             self.l_ri.angle_vector(self.l_robot_model.angle_vector(), time=1.5)
-        while self.l_ri.is_interpolating() or self.r_ri.is_interpolating():
-            rospy.sleep(0.1)
-
+        self.wait_interpolation()
